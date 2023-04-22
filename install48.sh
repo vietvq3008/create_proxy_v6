8e6b420363d8b16ed01c221f19107f7685077aa3
vietvq3008 <125423956+vietvq3008@users.noreply.github.com>
2023-04-22 08:31:25 +0700

    Create install48.sh

---
+++ install48.sh
@@ -0,0 +1,145 @@
+#!/bin/sh
+random() {
+	tr </dev/urandom -dc A-Za-z0-9 | head -c5
+	echo
+}
+
+array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
+gen48() {
+  ip48() {
+    echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
+  }
+  echo "$1:$(ip48):$(ip48):$(ip48):$(ip48):$(ip48)"
+}
+
+install_3proxy() {
+    echo "installing 3proxy"
+    URL="https://raw.githubusercontent.com/vietvqIT/proxyV6/main/3proxy-3proxy-0.8.6.tar.gz"
+    wget -qO- $URL | bsdtar -xvf-
+    cd 3proxy-3proxy-0.8.6
+    make -f Makefile.Linux
+    mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
+    cp src/3proxy /usr/local/etc/3proxy/bin/
+    cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
+    chmod +x /etc/init.d/3proxy
+    chkconfig 3proxy on
+    cd $WORKDIR
+}
+install_ndppd() {
+    cd ~ git clone --quiet https://github.com/DanielAdolfsson/ndppd.git >/dev/null
+    cd ~/ndppd
+    make -k all >/dev/null 2>&1
+    make -k install >/dev/null 2>&1
+    cat >~/ndppd/ndppd.conf <<END
+    route-ttl 30000
+    proxy he-ipv6 {
+       router no
+       timeout 500
+       ttl 30000
+       rule ${IP6}::/${PROXY_NET_MASK} {
+          static
+       }
+    }
+    END
+  cd $WORKDIR
+}
+
+gen_3proxy() {
+    cat <<EOF
+daemon
+maxconn 10000
+nscache 65536
+timeouts 1 5 30 60 180 1800 15 60
+setgid 65535
+setuid 65535
+flush
+auth strong
+
+users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
+
+$(awk -F "/" '{print "auth strong\n" \
+"allow " $1 "\n" \
+"proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
+"flush\n"}' ${WORKDATA})
+EOF
+}
+
+gen_proxy_file_for_user() {
+    cat >proxy.txt <<EOF
+$(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
+EOF
+}
+
+
+gen_data() {
+    seq $FIRST_PORT $LAST_PORT | while read port; do
+        echo "usr$(random)/pass$(random)/$IP4/$port/$(gen48 $IP6)"
+    done
+}
+
+gen_iptables() {
+    cat <<EOF
+    $(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
+EOF
+}
+
+gen_ifconfig() {
+    cat <<EOF
+$(awk -F "/" '{print "ifconfig enp1s0f0 inet6 add " $5 "/48"}' ${WORKDATA})
+EOF
+}
+echo "installing apps"
+yum -y install gcc net-tools bsdtar zip >/dev/null
+
+install_3proxy
+IP4=$(curl -4 -s icanhazip.com)
+IP6="2602:fe90:5a0"
+PROXY_NET_MASK="48"
+echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
+install_ndppd
+echo "working folder = /home/proxy-installer"
+WORKDIR="/home/proxy-installer"
+WORKDATA="${WORKDIR}/data.txt"
+mkdir $WORKDIR && cd $_
+
+
+
+
+
+echo "How many proxy do you want to create? Example 500"
+read COUNT
+
+FIRST_PORT=10000
+LAST_PORT=$(($FIRST_PORT + $COUNT))
+
+gen_data >$WORKDIR/data.txt
+gen_iptables >$WORKDIR/boot_iptables.sh
+gen_ifconfig >$WORKDIR/boot_ifconfig.sh
+chmod +x ${WORKDIR}/boot_*.sh /etc/rc.local
+
+gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
+
+cat >>/etc/rc.local <<EOF
+bash ${WORKDIR}/boot_iptables.sh
+bash ${WORKDIR}/boot_ifconfig.sh
+ulimit -n 10048
+ulimit -n 600000
+ulimit -u 600000
+ulimit -i 1200000
+ulimit -s 1000000
+ulimit -l 200000
+/sbin/ip addr add ${IP6}::/${PROXY_NET_MASK} dev he-ipv6
+sleep 5
+/sbin/ip -6 route add default via ${IP6}::1
+/sbin/ip -6 route add local ${IP6}::/${PROXY_NET_MASK} dev lo
+/sbin/ip tunnel add he-ipv6 mode sit remote ${IP4} local ${IP4} ttl 255
+/sbin/ip link set he-ipv6 up
+/sbin/ip -6 route add 2000::/3 dev he-ipv6
+~/ndppd/ndppd -d -c ~/ndppd/ndppd.conf
+sleep 2
+service 3proxy start
+EOF
+
+bash /etc/rc.local
+
+gen_proxy_file_for_user
