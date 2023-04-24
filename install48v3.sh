e4e5373d8e70f1e9a531f764a4a0dd645c2a5d29
vietvq3008 <125423956+vietvq3008@users.noreply.github.com>
2023-04-24 09:18:21 +0700

    Update and rename install48v2.sh to install48v3.sh

--- install48v3.sh
+++ install48v3.sh
@@ -29,18 +29,7 @@ if [[ ! "$TUNNEL_IPV4_ADDR" ]]; then
   exit 1
 fi
 
-####
-echo "↓ Proxies login (can be blank):"
-read PROXY_LOGIN
 
-if [[ "$PROXY_LOGIN" ]]; then
-  echo "↓ Proxies password:"
-  read PROXY_PASS
-  if [[ ! "PROXY_PASS" ]]; then
-    echo "● Proxies pass can't be emty"
-    exit 1
-  fi
-fi
 
 ####
 echo "↓ Port numbering start (default 1500):"
@@ -74,10 +63,7 @@ echo "● Host IPv4 address: $HOST_IPV4_ADDR"
 echo "● Tunnel IPv4 address: $TUNNEL_IPV4_ADDR"
 echo "● Proxies count: $PROXY_COUNT, starting from port: $PROXY_START_PORT"
 echo "● Proxies protocol: $PROXY_PROTOCOL"
-if [[ "$PROXY_LOGIN" ]]; then
-  echo "● Proxies login: $PROXY_LOGIN"
-  echo "● Proxies password: $PROXY_PASS"
-fi
+
 
 ####
 echo "-------------------------------------------------"
@@ -88,7 +74,7 @@ sudo yum -y install gcc g++ make bc pwgen git >/dev/null 2>&1
 ####
 echo ">-- Setting up sysctl.conf"
 cat >>/etc/sysctl.conf <<END
-net.ipv6.conf.eth0.proxy_ndp=1
+net.ipv6.conf.enp1s0f0.proxy_ndp=1
 net.ipv6.conf.all.proxy_ndp=1
 net.ipv6.conf.default.forwarding=1
 net.ipv6.conf.all.forwarding=1
@@ -139,6 +125,7 @@ sudo yum install git
 	END
 
 ####
+
 echo ">-- Setting up 3proxy"
 cd ~
 wget -q https://github.com/z3APA3A/3proxy/archive/0.8.13.tar.gz
@@ -151,63 +138,60 @@ touch src/define.txt
 echo "#define ANONYMOUS 1" >src/define.txt
 sed -i '31r src/define.txt' src/proxy.h
 make -f Makefile.Linux >/dev/null 2>&1
-cat >~/3proxy/3proxy.cfg <<END
-#!/bin/bash
 
+random() {
+	tr </dev/urandom -dc A-Za-z0-9 | head -c5
+	echo
+}
+
+LAST_PORT=$(($PROXY_START_PORT + $PROXY_COUNT))
+array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
+gen48() {
+  ip48() {
+    echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
+  }
+  echo "$1:$(ip48):$(ip48):$(ip48):$(ip48):$(ip48)"
+}
+gen_3proxy() {
+    cat <<EOF
 daemon
-maxconn 100
-nserver 1.1.1.1
+maxconn 10000
 nscache 65536
 timeouts 1 5 30 60 180 1800 15 60
 setgid 65535
 setuid 65535
-stacksize 6000
 flush
-END
-
-if [[ "$PROXY_LOGIN" ]]; then
-  cat >>~/3proxy/3proxy.cfg <<END
 auth strong
-users ${PROXY_LOGIN}:CL:${PROXY_PASS}
-allow ${PROXY_LOGIN}
-END
-else
-  cat >>~/3proxy/3proxy.cfg <<END
-auth none
-END
-fi
 
-####
-echo ">-- Generating IPv6 addresses"
-touch ~/ip.list
-touch ~/tunnels.txt
+users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
 
-P_VALUES=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
-PROXY_GENERATING_INDEX=1
-GENERATED_PROXY=""
-
-generate_proxy() {
-  a=${P_VALUES[$RANDOM % 16]}${P_VALUES[$RANDOM % 16]}${P_VALUES[$RANDOM % 16]}${P_VALUES[$RANDOM % 16]}
-  b=${P_VALUES[$RANDOM % 16]}${P_VALUES[$RANDOM % 16]}${P_VALUES[$RANDOM % 16]}${P_VALUES[$RANDOM % 16]}
-  c=${P_VALUES[$RANDOM % 16]}${P_VALUES[$RANDOM % 16]}${P_VALUES[$RANDOM % 16]}${P_VALUES[$RANDOM % 16]}
-  d=${P_VALUES[$RANDOM % 16]}${P_VALUES[$RANDOM % 16]}${P_VALUES[$RANDOM % 16]}${P_VALUES[$RANDOM % 16]}
-  e=${P_VALUES[$RANDOM % 16]}${P_VALUES[$RANDOM % 16]}${P_VALUES[$RANDOM % 16]}${P_VALUES[$RANDOM % 16]}
-
-  echo "$PROXY_NETWORK:$a:$b:$c:$d$([ $PROXY_NET_MASK == 48 ] && echo ":$e" || echo "")" >>~/ip.list
-
+$(awk -F "/" '{print "auth strong\n" \
+"allow " $1 "\n" \
+"$([ $PROXY_PROTOCOL == "socks5" ] && echo "socks" || echo "proxy") -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
+"flush\n"}' ${WORKDATA})
+EOF
 }
 
-while [ "$PROXY_GENERATING_INDEX" -le $PROXY_COUNT ]; do
-  generate_proxy
-  let "PROXY_GENERATING_INDEX+=1"
-done
+gen_proxy_file_for_user() {
+    cat >proxy.txt <<EOF
+$(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
+EOF
+}
 
-CURRENT_PROXY_PORT=${PROXY_START_PORT}
-for e in $(cat ~/ip.list); do
-  echo "$([ $PROXY_PROTOCOL == "socks5" ] && echo "socks" || echo "proxy") -6 -s0 -n -a -p$CURRENT_PROXY_PORT -i$HOST_IPV4_ADDR -e$e" >>~/3proxy/3proxy.cfg
-  echo "$PROXY_PROTOCOL://$([ "$PROXY_LOGIN" ] && echo "$PROXY_LOGIN:$PROXY_PASS@" || echo "")$HOST_IPV4_ADDR:$CURRENT_PROXY_PORT" >>~/tunnels.txt
-  let "CURRENT_PROXY_PORT+=1"
-done
+
+gen_data() {
+    seq $PROXY_START_PORT $LAST_PORT | while read port; do
+        echo "usr$(random)/pass$(random)/$HOST_IPV4_ADDR/$port/$(gen48 $PROXY_NETWORK)"
+    done
+}
+echo "working folder = /home/proxy-installer"
+WORKDIR="/home/proxy-installer"
+WORKDATA="${WORKDIR}/data.txt"
+mkdir $WORKDIR && cd $_
+gen_data >$WORKDIR/data.txt
+
+gen_3proxy > ~/3proxy/3proxy.cfg
+gen_proxy_file_for_user
 
 ####
 echo ">-- Setting up rc.local"
